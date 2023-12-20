75b215382f4fe70b12b809ae89e824c0353fa3a9
ryu1maniwa <ryu1maniwa@gmail.com>
2023-12-20 16:26:14 +0900

    import_sort

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +25,6 @@ class PixabayPage extends StatefulWidget {
 class _PixabayPageState extends State<PixabayPage> {
   @override
   Widget build(BuildContext context) {
-
     return const Scaffold();
   }
 }
